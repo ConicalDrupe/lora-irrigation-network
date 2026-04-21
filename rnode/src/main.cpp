b15775c691cdf7422ab41a120b777fd03d1d962b
ConicalDrupe <ChrisJacobBoon@gmail.com>
2026-04-20 21:38:42 -0500

    Improved usb setup until serial is ready or timeout occurs

--- rnode/src/main.cpp
+++ rnode/src/main.cpp
@@ -1,21 +1,42 @@
+#include <Reticulum.h>
+#include <Identity.h>
+#include <Destination.h>
+#include <Packet.h>
 #include <Arduino.h>
 
-#define Boot_Key 0
-#define Led      35
+
+const int BUTTON_PIN = 0; // cpp style istead of #define
+
+// Shared state for ISR (Interrupt Service Routine)
+// Use 'volatile' to prevent compiler from caching value
+// Do this for all variables related Interrupts
+volatile bool sendPacket = false;
+
+// Interrupt Service Routine
+// Use 'IRAM_ATTR' to put into fast on-chip RAM
+// Must be short function for reactivity - no logging, no delays, no LoRa calls...
+// Real work is done in the loop
+void IRAM_ATTR onButtonPress() {
+    sendPacket = true;
+};
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(Boot_Key, INPUT_PULLUP);
-  pinMode(Led, OUTPUT);
-}
+    Serial.begin(115200);
+    unsigned long t = millis();
+    while (!Serial && millis() - t < 3000) { delay(10); }
+
+    pinMode(BUTTON_PIN,INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), onButtonPress, FALLING);
+
+    Serial.println("Ready. Press PRG to send LoRa packet.");
+};
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(Boot_Key)==0) {
-    digitalWrite(Led,HIGH);
-    Serial.print("hello world !!! \n");
-    delay(1000);
-    digitalWrite(Led,LOW);
-  }
-  delay(100);
-}
+    if (sendPacket) {
+        sendPacket = false;
+
+        Serial.println("Button pressed. Sending LoRa packet.");
+        // code here
+    }
+};
